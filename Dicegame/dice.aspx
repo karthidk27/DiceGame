33f2314f4cb4159b463ff2e261f62e1389b30c85
karthi <karthi@KARTHIK>
2023-04-08 08:31:39 +0530

    Add project files.

---
+++ Dicegame/dice.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="dice.aspx.cs" Inherits="dice" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        body {
+               text-align:center;
+               background-image:url('img/bp.jpg');
+               background-repeat: no-repeat;
+               background-size: 100% 100%;
+               background-attachment: fixed;
+             }
+        #Button1
+        {
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+            background-color: #e7e7e7; 
+            color: black;
+            margin-top:15%;
+        }
+        
+        #Image2
+        {
+            margin-left:10%;
+        }
+        
+    </style>
+</head>
+<body>
+     <form id="form1" runat="server" >
+            <h1><asp:Label ID="Title" runat="server" Text="Dice Game" style="color:green;"></asp:Label></h1>
+            <br />
+            <asp:Image ID="Image1" runat="server" ImageUrl="~/img/1.jpg" />
+            <asp:Image ID="Image2" runat="server" ImageUrl="~/img/1.jpg" />
+            <br />
+
+
+
+            <h2><asp:Label ID="Status" runat="server" style="color:brown;"></asp:Label></h2>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Roll the Dice" style="" />
+   
+        
+    </form>
+</body>
+</html>
---
+++ Dicegame/dice.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class dice : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Status.Text = "Match Result";
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Random random = new Random();
+        int Rnd1 = random.Next(1, 6);
+        if (Rnd1 == 1)
+        {
+            Image1.ImageUrl = "~/img/1.jpg";
+        }
+        else if (Rnd1 == 2)
+        {
+            Image1.ImageUrl = "~/img/2.jpg";
+        }
+        else if (Rnd1 == 3)
+        {
+            Image1.ImageUrl = "~/img/3.jpg";
+        }
+        else if (Rnd1 == 4)
+        {
+            Image1.ImageUrl = "~/img/4.jpg";
+        }
+        else if (Rnd1 == 5)
+        {
+            Image1.ImageUrl = "~/img/5.jpg";
+        }
+        else
+        {
+            Image1.ImageUrl = "~/img/6.jpg";
+        }
+       
+        int Rnd2 = random.Next(1, 6);
+        if (Rnd2 == 1)
+        {
+            Image2.ImageUrl = "~/img/1.jpg";
+        }
+        else if (Rnd2 == 2)
+        {
+            Image2.ImageUrl = "~/img/2.jpg";
+        }
+        else if (Rnd2 == 3)
+        {
+            Image2.ImageUrl = "~/img/3.jpg";
+        }
+        else if (Rnd2 == 4)
+        {
+            Image2.ImageUrl = "~/img/4.jpg";
+        }
+        else if (Rnd2 == 5)
+        {
+            Image2.ImageUrl = "~/img/5.jpg";
+        }
+        else
+        {
+            Image2.ImageUrl = "~/img/6.jpg";
+        }
+        if (Rnd1 > Rnd2)
+        {
+            Status.Text = "Player 1 wins";
+        }
+        else if (Rnd2 > Rnd1)
+        {
+            Status.Text = "Player 2 Wins";
+        }
+        else
+        {
+            Status.Text = "Match draw";
+        }
+    }
+}
